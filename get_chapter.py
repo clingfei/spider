9a30413ce855795922cfb3ca0e31086309da0c6f
clingfei <1599101385@qq.com>
2021-03-27 13:09:22 +0800

    get a chapter of an novel

--- get_chapter.py
+++ get_chapter.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import get_catalog
+#import get_catalog
 
 def get_article(url, headers):
     html = requests.get(url, headers=headers).text
@@ -8,11 +8,11 @@ def get_article(url, headers):
     html = soup.prettify()
     return html
 
-def parse_html(html, title, chapter):
+def parse_html(html):
     soup = BeautifulSoup(html, 'lxml')
     results = soup.find_all("p")
-    path = title + "/" + chapter + ".txt"
-    file = open(path, "w", encoding='utf-8')
+    file = open("chapter.txt", "w", encoding='utf-8')
+    print(results)
     for result in results:
         text = result.text
         text = text.replace("\n", "")
@@ -22,10 +22,15 @@ def parse_html(html, title, chapter):
     file.close()
     print("写入完成")
 
-def getArr(arr, title):
-    headers = get_catalog.get_headers()
-    for params in arr:
-        url = params["url"]
-        chapter = params["serial_number_txt"]
-        html = get_article(url, headers=headers)
-        parse_html(html, title, chapter)
+if __name__ == "__main__":
+    url = "https://www.zhihu.com/market/paid_column/1238803218659819520/section/1238882552661647360"
+    file = open("cookies.txt", 'r', encoding='utf-8')
+    cookie = file.read()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.81',
+        'Host': 'www.zhihu.com',
+        'cookie': cookie
+    }
+    file.close()
+    html = get_article(url, headers)
+    parse_html(html)
